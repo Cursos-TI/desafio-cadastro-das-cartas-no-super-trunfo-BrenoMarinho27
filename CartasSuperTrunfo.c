0431c6a8c95fb7304372415649733da70e1d9924
BrenoMarinho27 <brenoanderoliver@gmail.com>
2025-02-11 23:11:15 +0000

    Comparativo de 2 cartas- Nivel mestre finalizado

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,51 +1,88 @@
 #include <stdio.h>
 
+// variáveis 
 int main() {
-    // Definindo variáveis 
-    int codigo;
-    char nome[20];
-    double populacao;
-    double area;
-    double pib;
-    int pontos;
+    char codigo1[20], codigo2[20];
+    char nome1[20], nome2[20];
+    unsigned long int populacao1, populacao2;
+    float area1, area2; 
+    float densidade1, densidade2;
+    float pib1, pib2;
+    float per_capita1, per_capita2;
+    int pontos1, pontos2;
+    float especial1, especial2;
 
-    // Perguntando os dados
-    printf("Qual o código da cidade?: ");
-    scanf("%d", &codigo);
-
+    // Colhendo dados da primeira carta
+    printf("\nDigite os dados da primeira carta:\n");
+    printf("Qual o código da carta? ");
+    scanf("%s", codigo1);
     printf("Qual o nome da cidade? ");
-    scanf("%s", nome);
-
-    printf("Qual a população?: ");
-    scanf("%f", &populacao);  // 
-
-    printf("Qual a área?: ");
-    scanf("%f", &area);  
-
+    scanf("%s", nome1);
+    printf("Qual a população? ");
+    scanf("%lu", &populacao1);
+    printf("Qual a área da cidade? "); 
+    scanf("%f", &area1);
     printf("Qual o PIB? ");
-    scanf("%f", &pib);  
-
+    scanf("%f", &pib1);
     printf("Quantos pontos turísticos? ");
-    scanf("%d", &pontos); 
+    scanf("%d", &pontos1);
 
-    //Calculando o pib per capita e a densidade populacional
+    // Calculando a Densidade e Per Capita da carta 1
+    densidade1 = populacao1 / area1;
+    per_capita1 = pib1 / populacao1;
+    
+    // Calculando o especial da carta 1
+    especial1 = populacao1 + area1 + pib1 + densidade1 + per_capita1 + pontos1;
+    
+    // Exibe os dados da primeira carta
+    printf("\nDados da Primeira Carta:\n");
+    printf("Código: %s\n", codigo1);
+    printf("Nome: %s\n", nome1);
+    printf("População: %lu\n", populacao1);
+    printf("Área: %.1f\n", area1);
+    printf("PIB: %f\n", pib1);
+    printf("Total de pontos turísticos: %d\n", pontos1);
+    printf("Densidade: %.1f\n", densidade1);
+    printf("Especial: %f\n", especial1);
+    
+    // Colhendo dados da segunda carta
+    printf("\nDigite os dados da segunda carta:\n");
+    printf("Qual o código da carta? ");
+    scanf("%s", &codigo2);
+    printf("Qual o nome da cidade? ");
+    scanf("%s", nome2);
+    printf("Qual a população? ");
+    scanf("%lu", &populacao2);
+    printf("Qual a área da cidade? "); 
+    scanf("%f", &area2);
+    printf("Qual o PIB? ");
+    scanf("%f", &pib2);
+    printf("Quantos pontos turísticos? ");
+    scanf("%d", &pontos2);
+    
+    // Calculando a Densidade e Per Capita da carta 2
+    densidade2 = populacao2 / area2;
+    per_capita2 = pib2 / populacao2;
+    
+    // Calculando o especial da carta 2
+    especial2 = populacao2 + area2 + pib2 + densidade2 + per_capita2 + pontos2;
+    
+    // Exibe os dados da segunda carta
+    printf("\nDados da Segunda Carta:\n");
+    printf("Código: %s\n", codigo2);
+    printf("Nome: %s\n", nome2);
+    printf("População: %lu\n", populacao2);
+    printf("Área: %.1f\n", area2);
+    printf("PIB: %f\n", pib2);
+    printf("Total de pontos turísticos: %d\n", pontos2);
+    printf("Densidade: %.1f\n", densidade2);
+    printf("Especial: %f\n", especial2);
+    
+    //Resultado da comparação das duas cartas:
+    printf("Densidade da carta 1 é menor do que a carta2?: %d\n", densidade1 < densidade2);
+   printf( "Especial da carta 1 é maior do que a carta2?:%d\n", especial1 > especial2);
 
-    double pib_per_capita = pib / populacao;
-    double densidade = populacao / area;
-
-    // Exibindo as respostas
-    printf("\nDados da carta:\n");
-    printf("Código: %d\n", codigo);
-    printf("Nome da cidade: %s\n", nome);
-    printf("População: %f habitantes\n", populacao);  // Corrigido: removido o acento
-    printf("Área: %f Km²\n", area);
-    printf("PIB: %f bilhões\n", pib);
-    printf("Pontos turísticos:%d\n", pontos);  
-
-//Exibindo resultado dos cálculos
-
-    printf("O per capita da cidade é: %.2f\n", pib_per_capita);
-    printf(" A densidade da cidade é: %.2f\n", densidade);
-
+   
+    
     return 0;
 }
